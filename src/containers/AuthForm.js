56027c890b96f9a6f658ae18548996bc031a9984
Martin Schulz <martischulzofficial@gmail.com>
2019-03-24 03:28:56 +0500

    auth form errors

--- src/containers/AuthForm.js
+++ src/containers/AuthForm.js
@@ -2,21 +2,44 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import { authorizeAction, restorePassrwordAction } from '../actions/actions';
+
 import './AuthForm.scss';
+
+const getErrorMessage = ({ error, restoreError, noLogin }) => {
+  if (error) {
+    return 'Неправильный логин или пароль';
+  }
+  if (restoreError) {
+    return 'Пользователя с такой почтой или телефоном не существует';
+  }
+  if (noLogin) {
+    return 'Введите эл. почту или телефон';
+  }
+  return '';
+};
 
 class AuthForm extends Component {
   state = {
-    errorMessage: '',
     login: '',
-    password: ''
+    password: '',
+    noLogin: false
   }
 
   restorePassword = () => {
-
+    const { login, password } = this.state;
+    if (!login) {
+      this.setState({ noLogin: true });
+      return;
+    }
+    this.setState({ noLogin: false });
   }
 
   send = () => {
-
+    const { login, password } = this.state;
+    const { authorizeAction } = this.props;
+    this.setState({ noLogin: false });
+    authorizeAction({ login, password });
   }
 
   onChange = (event) => {
@@ -29,8 +52,13 @@ class AuthForm extends Component {
 
   render() {
     const {
-      errorMessage, login, password
+      login, password, noLogin
     } = this.state;
+    const {
+      loading, error, restoreError
+    } = this.props;
+
+    const errorMessage = getErrorMessage({ error, restoreError, noLogin });
 
     return <div className="auth-form-wrap">
       <form className="auth-form-content">
@@ -89,5 +117,10 @@ class AuthForm extends Component {
 
 export default connect((state) => {
   return {
+    error: state.user.error,
+    restoreError: state.user.restoreError,
+    loading: state.user.loading
   }
-}, {})(AuthForm);
+}, {
+  authorizeAction
+})(AuthForm);
